f94f6ebb2638be52dd20922499f1bf6ebffebd34
Blueberryy <igory.ygr200@gmail.com>
2020-10-03 12:29:49 +0500

    Create cultassassineng.lua

---
+++ lua/lang/russian/cultassassineng.lua
@@ -0,0 +1,11 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[CULTASSASSIN.name] = "Убийца культа"
+L["info_popup_" .. CULTASSASSIN.name] = [[Вы - убийца культа, вы - самый опытный убийца культов!]]
+L["body_found_" .. CULTASSASSIN.abbr] = "Он был убийцей культа."
+L["search_role_" .. CULTASSASSIN.abbr] = "Этот человек был убийцей культа!"
+L["target_" .. CULTASSASSIN.name] = "Убийца культа"
+L["ttt2_desc_" .. CULTASSASSIN.name] = [[Убийца культа - это культовый человек, который избавляется от надоедливых противников, у него есть инструменты для работы, и он выполнит свою работу должным образом.]]
+
+-- OTHER ROLE LANGUAGE STRINGS
